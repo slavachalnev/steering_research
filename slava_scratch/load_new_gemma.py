e6a51500d657d160fae7401ac76f490ea16e053a
Slava <slava.chalnev@gmail.com>
2024-08-02 20:42:57 +0000

    Refactor SAE

--- slava_scratch/load_new_gemma.py
+++ slava_scratch/load_new_gemma.py
@@ -3,6 +3,11 @@ import os
 import sys
 sys.path.append(os.path.abspath('..'))
 
+from steering.patch import patch_resid
+from steering.sae import JumpReLUSAE
+
+from functools import partial
+
 import torch
 import torch.nn as nn
 from transformer_lens import HookedTransformer
@@ -19,6 +24,7 @@ model = HookedTransformer.from_pretrained("google/gemma-2-2b", device=device)
 
 # %%
 
+hp = "blocks.12.hook_resid_post"
 path_to_params = hf_hub_download(
     repo_id="google/gemma-scope-2b-pt-res",
     filename="layer_12/width_16k/average_l0_82/params.npz",
@@ -28,41 +34,33 @@ path_to_params = hf_hub_download(
 params = np.load(path_to_params)
 pt_params = {k: torch.from_numpy(v).cuda() for k, v in params.items()}
 
-# %%
-class JumpReLUSAE(nn.Module):
-  def __init__(self, d_model, d_sae):
-    # Note that we initialise these to zeros because we're loading in pre-trained weights.
-    # If you want to train your own SAEs then we recommend using blah
-    super().__init__()
-    self.W_enc = nn.Parameter(torch.zeros(d_model, d_sae))
-    self.W_dec = nn.Parameter(torch.zeros(d_sae, d_model))
-    self.threshold = nn.Parameter(torch.zeros(d_sae))
-    self.b_enc = nn.Parameter(torch.zeros(d_sae))
-    self.b_dec = nn.Parameter(torch.zeros(d_model))
-
-  def encode(self, input_acts):
-    pre_acts = input_acts @ self.W_enc + self.b_enc
-    mask = (pre_acts > self.threshold)
-    acts = mask * torch.nn.functional.relu(pre_acts)
-    return acts
-
-  def decode(self, acts):
-    return acts @ self.W_dec + self.b_dec
-
-  def forward(self, acts):
-    acts = self.encode(acts)
-    recon = self.decode(acts)
-    return recon
-
-# %%
-
 sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
 sae.load_state_dict(pt_params)
 sae.to(device)
 
 # %%
 
+london = sae.W_dec[14455]
+uk = sae.W_dec[3931]
+place = sae.W_dec[865]
+
+sf = sae.W_dec[3124]
+
+marriage = sae.W_dec[2421]
+wedding = sae.W_dec[4230]
+
+
+def gen(prompt, steer, scale):
+    toks = model.to_tokens(prompt, prepend_bos=True)
+    toks = toks.expand(10, -1)
+    with model.hooks([(hp, partial(patch_resid, steering=steer, scale=scale))]):
+        gen_toks = model.generate(toks, max_new_tokens=30)
+    return model.to_string(gen_toks)
+
+# %%
+gen("I think", wedding, 80)
 
 
 # %%
+
 # %%
--- slava_scratch/run_ff.py
+++ slava_scratch/run_ff.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.abspath('..'))
 import json
+from steering.sae import JumpReLUSAE
 
 import torch
 import torch.nn as nn
@@ -9,6 +10,7 @@ from torch.utils.data import DataLoader
 from transformer_lens import HookedTransformer
 from transformer_lens import utils as tutils
 from transformer_lens.evals import make_pile_data_loader, evaluate_on_dataset
+
 
 from functools import partial
 from datasets import load_dataset
@@ -35,31 +37,6 @@ path_to_params = hf_hub_download(
 
 params = np.load(path_to_params)
 pt_params = {k: torch.from_numpy(v).cuda() for k, v in params.items()}
-
-class JumpReLUSAE(nn.Module):
-  def __init__(self, d_model, d_sae):
-    # Note that we initialise these to zeros because we're loading in pre-trained weights.
-    # If you want to train your own SAEs then we recommend using blah
-    super().__init__()
-    self.W_enc = nn.Parameter(torch.zeros(d_model, d_sae))
-    self.W_dec = nn.Parameter(torch.zeros(d_sae, d_model))
-    self.threshold = nn.Parameter(torch.zeros(d_sae))
-    self.b_enc = nn.Parameter(torch.zeros(d_sae))
-    self.b_dec = nn.Parameter(torch.zeros(d_model))
-
-  def encode(self, input_acts):
-    pre_acts = input_acts @ self.W_enc + self.b_enc
-    mask = (pre_acts > self.threshold)
-    acts = mask * torch.nn.functional.relu(pre_acts)
-    return acts
-
-  def decode(self, acts):
-    return acts @ self.W_dec + self.b_dec
-
-  def forward(self, acts):
-    acts = self.encode(acts)
-    recon = self.decode(acts)
-    return recon
 
 sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
 sae.load_state_dict(pt_params)
