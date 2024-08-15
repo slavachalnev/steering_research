f27bc1923c2d9df42b2a0de3d45209020bd6bca5
Sviatoslav <slava.chalnev@gmail.com>
2024-08-15 15:21:55 +1000

    32k sae features

--- ft_effects/multi_gpu.py
+++ ft_effects/multi_gpu.py
@@ -148,9 +148,29 @@ def main(features, save_dir):
     print('done save')
 
 if __name__ == "__main__":
+    # # 16k
+    # path_to_params = hf_hub_download(
+    # repo_id="google/gemma-scope-2b-pt-res",
+    # filename="layer_12/width_16k/average_l0_82/params.npz",
+    # force_download=False)
+    # params = np.load(path_to_params)
+    # pt_params = {k: torch.from_numpy(v) for k, v in params.items()}
+    # sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
+    # sae.load_state_dict(pt_params)
+    # sae._requires_grad = False
+
+    # save_dir = "effects/G2_2B_L12/multi_16k_from_0"
+    # os.makedirs(save_dir)
+    # main(sae.W_dec, save_dir)
+
+    ###################################3
+
+
+    # 32k
+    # layer_12/width_32k/average_l0_76
     path_to_params = hf_hub_download(
     repo_id="google/gemma-scope-2b-pt-res",
-    filename="layer_12/width_16k/average_l0_82/params.npz",
+    filename="layer_12/width_32k/average_l0_76/params.npz",
     force_download=False)
     params = np.load(path_to_params)
     pt_params = {k: torch.from_numpy(v) for k, v in params.items()}
@@ -158,6 +178,7 @@ if __name__ == "__main__":
     sae.load_state_dict(pt_params)
     sae._requires_grad = False
 
-    save_dir = "effects/G2_2B_L12/multi_16k_from_0"
+    save_dir = "effects/G2_2B_L12/multi_32k_from_0"
     os.makedirs(save_dir)
     main(sae.W_dec, save_dir)
+
