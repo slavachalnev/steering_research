394c08529b2cdadf5c7c180bb91a744f02a12f70
Sviatoslav <slava.chalnev@gmail.com>
2024-09-24 17:44:30 +1000

    load effects

--- ft_effects/load_effects.py
+++ ft_effects/load_effects.py
@@ -8,60 +8,58 @@ import einops
 
 
 # %%
-path_a = "effects/G2_2B_L12/16k_from_0"
-features_a = torch.load(os.path.join(path_a, "used_features.pt"))
-effects_a = torch.load(os.path.join(path_a, "all_effects.pt"))
-# %%
-path_b = "effects/G2_2B_L12/multi_16k_from_0"
-features_b = torch.load(os.path.join(path_b, "used_features.pt"))
-effects_b = torch.load(os.path.join(path_b, "all_effects.pt"))
-# %%
-# c is shuffled a
-effects_c = effects_a[torch.randperm(effects_a.shape[0])]
-# %%
+paths = [
+    "effects/G2_2B_L12/65k_from_0",
+    "effects/G2_2B_L12/65k_from_10k",
+    "effects/G2_2B_L12/65k_from_20k",
+    "effects/G2_2B_L12/65k_from_30k",
+    "effects/G2_2B_L12/65k_from_40k",
 
-norms_a = torch.norm(features_a, dim=-1)
-norms_b = torch.norm(features_b, dim=-1)
+    # "effects/G2_2B_L12/16k_from_0",
+    # "effects/G2_2B_L12/sample_and_combine_16k",
 
-fig = px.histogram(norms_a, nbins=50)
-fig.show()
-fig = px.histogram(norms_b, nbins=50)
-fig.show()
+    # "effects/G2_2B_L12/random",
+    # "effects/G2_2B_L12/random_2",
+    # "effects/G2_2B_L12/random_3",
+    # "effects/G2_2B_L12/random_4",
+    # "effects/G2_2B_L12/random_5",
+]
 
-# %%
+features = []
+effects = []
 
-print(torch.all(features_a == features_b))
-print(torch.all(effects_a == effects_b))
-print(torch.allclose(features_a, features_b))
-print(torch.allclose(effects_a, effects_b))
+for path in paths:
+    features.append(torch.load(os.path.join(path, "used_features.pt")))
+    effects.append(torch.load(os.path.join(path, "all_effects.pt")))
 
-# %%
-# cosine similarity between effects a and b
-
-sims = einops.einsum(effects_a, effects_b,"d_sae d_model, d_sae d_model -> d_sae") / (torch.norm(effects_a, dim=-1) * torch.norm(effects_b, dim=-1))
-print(sims.shape)
-print(sims)
-
-fig = px.histogram(sims)
-fig.show()
+features = torch.cat(features)
+effects = torch.cat(effects)
 
 # %%
-sims = einops.einsum(effects_a, effects_c,"d_sae d_model, d_sae d_model -> d_sae") / (torch.norm(effects_a, dim=-1) * torch.norm(effects_c, dim=-1))
-fig = px.histogram(sims)
+
+# compute sum of absolute values of effects
+effects_abs = effects.abs().sum(dim=0)
+effects_abs[:20]
+
+# %%
+
+# plot histogram of effects_abs
+
+fig = px.histogram(effects_abs)
 fig.show()
 
 
-# %%
 
-for i in range(3):
-    xs = effects_a[:, i]
-    ys = effects_b[:, i]
-    fig = px.scatter(x=xs, y=ys, opacity=0.5)  # Set opacity to 0.5 for half-translucent datapoints
-    fig.show()
 
 # %%
-
-print(torch.norm(effects_a, dim=-1))
-print(torch.norm(effects_b, dim=-1))
+# feature id 115
+effects_abs[115]
 
 # %%
+# get bottom 10 features
+bottom_10 = effects_abs.argsort()[:10]
+print(bottom_10)
+print(effects_abs[bottom_10])
+
+# %%
+
