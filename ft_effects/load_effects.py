Fix loss calculation and calibration

ft_effects/load_effects.py
@@ -15,7 +15,7 @@ paths = [ # small model
     # "effects/G2_2B_L12/65k_from_30k",
     # "effects/G2_2B_L12/65k_from_40k",
 
-    # "effects/G2_2B_L12/16k_from_0",
+    "effects/G2_2B_L12/16k_from_0",
     # "effects/G2_2B_L12/sample_and_combine_16k",
 
     # "effects/G2_2B_L12/random",
@@ -25,10 +25,10 @@ paths = [ # small model
     # "effects/G2_2B_L12/random_5",
 ]
 
-paths = [ # big model
-    "effects/G2_9B_L12/131k_from_0",
-    "effects/G2_9B_L12/131k_from_64k",
-]
+# paths = [ # big model
+#     "effects/G2_9B_L12/131k_from_0",
+#     "effects/G2_9B_L12/131k_from_64k",
+# ]
 
 features = []
 effects = []
@@ -39,6 +39,12 @@ for path in paths:
 
 features = torch.cat(features)
 effects = torch.cat(effects)
+
+# %%
+
+# norm of first 10 features
+print(features[:10].norm(dim=-1))
+
 # %%
 print(features.shape)
 print(effects.shape)
@@ -72,4 +78,5 @@ print(bottom_10)
 print(effects_abs[bottom_10])
 
 # %%
+
 

ft_effects/multi_gpu.py
@@ -15,6 +15,7 @@ from steering.sae import JumpReLUSAE
 from huggingface_hub import hf_hub_download
 import numpy as np
 from queue import Empty
+import time
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
@@ -75,6 +76,7 @@ def worker(rank, world_size, task_queue, features, save_dir, big_model, scale=No
                 feature_index = task_queue.get(timeout=1)  # 1 second timeout
             except Empty:
                 break
+            start_time = time.time()  # Start timing
             feature = features[feature_index]
             if scale is None:
                 if big_model:
@@ -83,14 +85,14 @@ def worker(rank, world_size, task_queue, features, save_dir, big_model, scale=No
                                           loader=loader,
                                           scales=list(range(0, 220, 20)),
                                           n_batches=8,
-                                          target_loss=3,
+                                          target_loss=3.5,
                                           )
                 else:
                     opt_scale = get_scale(model=model,
                                           steer=feature.to(sae.W_dec.device),
                                           loader=loader,
                                           scales=list(range(0, 220, 20)),
-                                          target_loss=6,
+                                          target_loss=4,
                                           )
             else:
                 opt_scale = scale
@@ -106,7 +108,8 @@ def worker(rank, world_size, task_queue, features, save_dir, big_model, scale=No
                 'used_feature': (feature * opt_scale).cpu(),
                 'feature_index': feature_index
             })
-            print(f"processed {feature_index}")
+            elapsed_time = time.time() - start_time  # Calculate elapsed time
+            print(f"processed {feature_index} in {elapsed_time:.2f} seconds")
     finally:
         torch.save(results, os.path.join(save_dir, f"partial_results_rank_{rank}.pt"))
         cleanup()
@@ -254,54 +257,54 @@ if __name__ == "__main__":
     # steering_vectors = steering_vectors / steering_vectors.norm(dim=-1, keepdim=True)
     # main(steering_vectors, save_dir)
     
-    # ###################################
-    # # # 9b
-    # save_dir = "effects/G2_9B_L12/131k_from_0"
-    # os.makedirs(save_dir)
-
-    # # 131k
-    # path_to_params = hf_hub_download(
-    #     repo_id="google/gemma-scope-9b-pt-res",
-    #     filename="layer_12/width_131k/average_l0_96/params.npz",
-    #     force_download=False)
-    # params = np.load(path_to_params)
-    # pt_params = {k: torch.from_numpy(v) for k, v in params.items()}
-    # sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
-    # sae.load_state_dict(pt_params)
-    # sae._requires_grad = False
-
-    # input_features = sae.W_dec[:32000]
-
-    # main(input_features, save_dir, big_model=True)
-
-
-    # ###################################
-    # # 2b
-    # # load adapter. Then find the 16k features that have the biggest effects
-    # # steer with the optimised features.
-    
-    save_dir = "effects/G2_2B_L12/optimised_16k"
+    ###################################
+    # # 9b
+    save_dir = "effects/G2_9B_L12/131k_from_0_fixloss"
     os.makedirs(save_dir)
 
-    from ft_effects.utils import LinearAdapter, get_sae
-    sae = get_sae()
+    # 131k
+    path_to_params = hf_hub_download(
+        repo_id="google/gemma-scope-9b-pt-res",
+        filename="layer_12/width_131k/average_l0_96/params.npz",
+        force_download=False)
+    params = np.load(path_to_params)
+    pt_params = {k: torch.from_numpy(v) for k, v in params.items()}
+    sae = JumpReLUSAE(params['W_enc'].shape[0], params['W_enc'].shape[1])
+    sae.load_state_dict(pt_params)
+    sae._requires_grad = False
 
-    # load adapter
-    adapter = LinearAdapter(sae.W_enc.shape[0], sae.W_enc.shape[1])
-    adapter.load_state_dict(torch.load("linear_adapter.pt"))
-    adapter.cpu()
+    input_features = sae.W_dec[:32000]
 
-    b = adapter.W @ adapter.b
-    b = b/torch.norm(b)
-    input_features = []
-    for f in adapter.W.T:
-        f_to_add = f/torch.norm(f) - b
-        f_to_add = f_to_add/torch.norm(f_to_add)
-        input_features.append(f_to_add)
-    input_features = torch.stack(input_features)
-    print(input_features.shape)
+    main(input_features, save_dir, big_model=True)
 
-    main(input_features, save_dir)
+
+    # # ###################################
+    # # # 2b
+    # # # load adapter. Then find the 16k features that have the biggest effects
+    # # # steer with the optimised features.
+    
+    # save_dir = "effects/G2_2B_L12/optimised_16k"
+    # os.makedirs(save_dir)
+
+    # from ft_effects.utils import LinearAdapter, get_sae
+    # sae = get_sae()
+
+    # # load adapter
+    # adapter = LinearAdapter(sae.W_enc.shape[0], sae.W_enc.shape[1])
+    # adapter.load_state_dict(torch.load("linear_adapter.pt"))
+    # adapter.cpu()
+
+    # b = adapter.W @ adapter.b
+    # b = b/torch.norm(b)
+    # input_features = []
+    # for f in adapter.W.T:
+    #     f_to_add = f/torch.norm(f) - b
+    #     f_to_add = f_to_add/torch.norm(f_to_add)
+    #     input_features.append(f_to_add)
+    # input_features = torch.stack(input_features)
+    # print(input_features.shape)
+
+    # main(input_features, save_dir)
 
 
 

ft_effects/utils.py
@@ -109,7 +109,7 @@ def get_scale(
         loader,
         scales,
         n_batches=2,
-        target_loss=6,
+        target_loss=4,
         hp="blocks.12.hook_resid_post",
     ):
     assert torch.allclose(torch.norm(steer), torch.tensor(1.0))
@@ -120,7 +120,7 @@ def get_scale(
             with model.hooks(fwd_hooks=[(hp, partial(patch_resid, steering=steer, scale=scale))]):
                 loss = model(batch['tokens'], return_type='loss')
                 total_loss += loss
-            if batch_idx >= n_batches:
+            if batch_idx >= n_batches - 1:
                 break
         losses.append(total_loss.item() / n_batches)
         if total_loss.item()/n_batches > target_loss:

slava_scratch/loss_vs_coherence.py
@@ -26,7 +26,7 @@ from huggingface_hub import hf_hub_download, HfFileSystem
 torch.set_grad_enabled(False)
 
 # Set the global variable BIG_MODEL
-BIG_MODEL = False  # Set to True for the 9B model, False for the 2B model
+BIG_MODEL = True  # Set to True for the 9B model, False for the 2B model
 
 # %%
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -124,7 +124,16 @@ def compute_loss(steer, scales, n_batches):
 
 # %%
 
+# # loss for ft_id 2822 -- big model wedding feature.
+# steer = sae.W_dec[2822].to(device)
+# losses = compute_loss(steer, scales, n_batches=4)
+# print(scales)
+# print(losses)
+
+# %%
+
 def compute_all(n=100):
+    n_batches = n_samples // batch_size
     all_coherences: list[list[float]] = []
     all_losses: list[list[float]] = []
 
@@ -132,7 +141,7 @@ def compute_all(n=100):
         steer = sae.W_dec[i].to(device)
 
         cohs: list[float] = coherence(steer, scales)
-        losses = compute_loss(steer, scales, n_batches=1)
+        losses = compute_loss(steer, scales, n_batches=n_batches)
 
         all_coherences.append(cohs)
         all_losses.append(losses)
@@ -141,7 +150,7 @@ def compute_all(n=100):
 
 # %%
 
-all_c, all_l = compute_all(10)
+all_c, all_l = compute_all(7)
 
 # %%
 